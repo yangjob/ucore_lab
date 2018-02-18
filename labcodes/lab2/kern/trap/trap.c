52ca462e276ab46cb90eb05235c211dff4a0ba66
yangjob <923064497@qq.com>
2018-02-18 17:23:35 +0800

    delete a error in trap.c from lab1

--- labcodes/lab2/kern/trap/trap.c
+++ labcodes/lab2/kern/trap/trap.c
@@ -57,7 +57,6 @@ idt_init(void) {
         for(i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++){
                 SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
         }
-       k
         SETGATE(idt[T_SWITCH_TOK], 0, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
 
         //load the IDT，和lgdt相似？
